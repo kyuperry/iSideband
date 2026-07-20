e3bd844097c6d0f36caccf316fbfedbe68c902e2
Kyle Perry <kyleperry97@yahoo.com>
2026-07-19 16:46:22 -1000

    Initial Commit

---
+++ iSideband/iSidebandApp.swift
@@ -0,0 +1,17 @@
+//
+//  iSidebandApp.swift
+//  iSideband
+//
+//  Created by Kyle Perry on 7/19/26.
+//
+
+import SwiftUI
+
+@main
+struct iSidebandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
